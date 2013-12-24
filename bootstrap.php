b021aba017955483df8a0c6c4e2a2a6c912575b5
Batikan Turkmen <batikant@gmail.com>
2013-12-24 21:20:20 +0200

    bootstrap php

---
+++ bootstrap.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
